37ac800fe6dfd94e0531260aa357298f0308d8ad
CalebMartim <calebmartim2113@gmail.com>
2021-12-17 16:30:00 -0300

    testimonials
    Added the first file that will contain all testimonial details and that will be added to the page using a map function later

---
+++ src/Testimonials.js
@@ -0,0 +1,30 @@
+export const Testimonials = [
+  {
+    id: 1,
+    photo: "./People/Edward.jpg",
+    name: "",
+    testimonial: "",
+    position,
+  },
+  {
+    id: 2,
+    photo: "",
+    name: "",
+    testimonial: "",
+    position,
+  },
+  {
+    id: 3,
+    photo: "",
+    name: "",
+    testimonial: "",
+    position,
+  },
+  {
+    id: 4,
+    photo: "./People/Bruce.jpg",
+    name: "",
+    testimonial: "",
+    position,
+  },
+];
