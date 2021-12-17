6498790c9e041b71f0e9a2faa75b8cab6c9f3e33
CalebMartim <calebmartim2113@gmail.com>
2021-12-17 16:28:39 -0300

    responsiveness update
    made more responsiveness updates, header is essencially done now, displaying nicely on all screen sizes

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import "./Home.css";
 import iphoneSS from "./Iphone_Screenshot.png";
 import pixelSS from "./Pixel_Screenshot.png";
+import Bruce from "./People/Bruce.jpg";
 
 function Home() {
   return (
@@ -8,16 +9,50 @@ function Home() {
       <main>
         <section className="intro">
           <div className="intro_content">
-            <h1>Speak Freely</h1>
-            <p>
-              Say "hello" to a different messaging experience. An unexpected
-              focus on privacy, combined with all of the features you expect.
-            </p>
-            <button className="getSignalBtn">Get Signal</button>
+            <div>
+              <h1 className="spaced">Speak Freely</h1>
+              <p className="spaced">
+                Say "hello" to a different messaging experience. An unexpected
+                focus on privacy, combined with all of the features you expect.
+              </p>
+              <div className="spaced">
+                <button className="getSignalBtn">Get Signal</button>
+              </div>
+            </div>
           </div>
           <div className="intro_images">
             <img src={iphoneSS} alt="Screenshot of the app from an Iphone" />
             <img src={pixelSS} alt="Screenshot of the app from a Pixel phone" />
+          </div>
+        </section>
+        <section className="testimonials">
+          <div className="testimonial">
+            <img src="" alt="" />
+            <p></p>
+            <p>
+              <span></span>
+            </p>
+          </div>
+          <div className="testimonial">
+            <img src={Bruce} alt="" />
+            <p></p>
+            <p>
+              <span></span>
+            </p>
+          </div>
+          <div className="testimonial">
+            <img src="" alt="" />
+            <p></p>
+            <p>
+              <span></span>
+            </p>
+          </div>
+          <div className="testimonial">
+            <img src="" alt="" />
+            <p></p>
+            <p>
+              <span></span>
+            </p>
           </div>
         </section>
       </main>
